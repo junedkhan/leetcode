9a1fba4102f1340cd7a111539e9694c8b3d3b47a
Juned Khan <junedkhan933@gmail.com>
2025-04-04 20:07:06 +0530

    Time: 41 ms (39.29%) | Memory: 73.6 MB (29.76%) - LeetSync

---
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} paths
+ * @return {string[][]}
+ */
+var findDuplicate = function(paths) {
+    const map = {};
+    paths.forEach((item) => {
+        const splitted = item.split(" ");
+        for(let i = 1; i<splitted.length; i++) {
+            const moreSplit = splitted[i].split(".")
+            if(!map[moreSplit[1]]) {
+                map[moreSplit[1]] = [`${splitted[0]}/${moreSplit[0]}.txt`]
+            } else {
+                map[moreSplit[1]].push(`${splitted[0]}/${moreSplit[0]}.txt`)
+            }
+        }
+    })
+
+    const result = [];
+
+    return Object.values(map).filter((item) => {
+        return item.length > 1;
+    });
+};
