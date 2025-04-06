3a03c7e773cbfebe624529f209d3951e2bb5bc44
Juned Khan <junedkhan933@gmail.com>
2025-04-06 13:59:21 +0530

    Time: 1001 ms (8.70%) | Memory: 71.2 MB (13.04%) - LeetSync

---
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word
+ * @param {number} k
+ * @return {number}
+ */
+var minimumOperationsToMakeKPeriodic = function(word, k) {
+    const map = new Map();
+    let total = 0;
+    let maxOcc = 0;
+
+    for(let i = 0; i < word.length; i+=k) {
+        const subStr = word.slice(i, i + k);
+        console.log("subStr", subStr)
+        if(map.get(subStr)) {
+            let val = map.get(subStr);
+            map.set(subStr, ++val)
+        } else {
+            map.set(subStr, 1)
+        }
+
+        maxOcc = Math.max(maxOcc, map.get(subStr))
+        total += 1;
+    }
+
+    console.log("map", map)
+
+    return total - maxOcc;
+};
