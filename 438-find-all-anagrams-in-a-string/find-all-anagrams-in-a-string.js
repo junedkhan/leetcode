2200f8b2e2500dfb1fe07eb58b5c347fae997fc5
Juned Khan <junedkhan933@gmail.com>
2025-04-11 12:02:53 +0530

    Time: 12 ms (85.47%) | Memory: 61.3 MB (45.49%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.js
@@ -4,37 +4,50 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
-    let list = [];
-    let freqS = new Array(26).fill(0);
-    let freqP = new Array(26).fill(0);
-
-    if (s.length < p.length) {
-        return list;
-    }
-
-    for (let i = 0; i < p.length; i++) {
-        freqS[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-        freqP[p.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-    }
-
-    let start = 0;
-    let end = p.length;
-
-    if (freqS.join('') === freqP.join('')) {
-        list.push(start);
-    }
-
-    while (end < s.length) {
-        freqS[s.charCodeAt(start) - 'a'.charCodeAt(0)]--;
-        freqS[s.charCodeAt(end) - 'a'.charCodeAt(0)]++;
-
-        if (freqS.join('') === freqP.join('')) {
-            list.push(start + 1);
-        }
-
-        start++;
-        end++;
+    // initialize output array to be returned at the end and neededChars object to store the chars in p.
+    const output = [];
+    const neededChars = {};
+    
+    // populate neededChars to contain every char in p as a key and how many times that char appears in p as its value.
+    for (let char of p) {
+        if (char in neededChars) {
+            neededChars[char]++
+        } else neededChars[char] = 1
     }
     
-    return list;
+    // initialize window pointers and the total number of chars needed to form an anagram.
+    let left = 0;
+    let right = 0; 
+    let count = p.length 
+    
+    // start sliding the window
+    while (right < s.length) {
+        
+        // if the current char is found in p and is currently needed (meaning that its value in neededChars is bigger than 0), 
+        // then decrease the count which is the total number of chars that are needed and that still haven't been found.
+        if (neededChars[s[right]] > 0) count--;
+        
+        // decrease the needed amount for the current char and move the window's right end one step forward.
+        neededChars[s[right]]--;
+        right++;
+        
+        // if the count is 0, this means that there is an anagram starting at the left index so push left into the output array.
+        if (count === 0) output.push(left);
+        
+        // at first, the window will increase its length by taking steps forward with its right end.
+        // after the window length reaches p's length for the first time,
+		// the window will start moving forward like a caterpillar with the left end moving first. 
+        if (right - left == p.length) {
+            
+            // if the char left behind was a needed char, increase the total number of chars currently needed to form an anagram.
+            if (neededChars[s[left]] >= 0) count++;
+            
+            // the lines below are the most important to understand: 
+            // every time a needed char is left behind (outside the window) as the window moves forward to search the rest of the string, 
+            // increment that char's value in the neededChars object (restore the need for that char for the window's future reference).
+            neededChars[s[left]]++;
+            left++;
+        }
+    }
+    return output;
 };
