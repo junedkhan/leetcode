ead595b3a3360b05942b49e78a9d0f20d2188217
Juned Khan <junedkhan933@gmail.com>
2025-04-24 23:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (28.78%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if (!head) return null;
+
+    let current = head;
+
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+};
