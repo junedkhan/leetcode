c4e56f8c75cc32cd7ff9314c01d4013696184cd0
Juned Khan <junedkhan933@gmail.com>
2025-06-05 11:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (12.81%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        
+        if (nums[mid] < nums[right]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return nums[left];
+};
