cad14fade5e5b5034dd1cedc3fb360a1acf430bb
Juned Khan <junedkhan933@gmail.com>
2025-07-07 14:43:40 +0530

    Time: 7 ms (99.92%) | Memory: 67.6 MB (58.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,24 +3,24 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    const primes = [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+        31, 37, 41, 43, 47, 53, 59, 61, 67,
+        71, 73, 79, 83, 89, 97, 101
+    ];
+
     const map = new Map();
 
     for (const str of strs) {
-        // Create a frequency array of size 26
-        const count = new Array(26).fill(0);
+        let hash = 1;
         for (const char of str) {
-            count[char.charCodeAt(0) - 97]++;
+            hash *= primes[char.charCodeAt(0) - 97];
         }
 
-        // Use the count array as a string key
-        const key = count.join('#'); // '#' to avoid ambiguity
-
-        if (!map.has(key)) {
-            map.set(key, []);
-        }
-
-        map.get(key).push(str);
+        if (!map.has(hash)) map.set(hash, []);
+        map.get(hash).push(str);
     }
 
     return Array.from(map.values());
 };
+
